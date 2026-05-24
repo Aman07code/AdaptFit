6b569102b799326e9a4c41fb45e58f9459a5e869
Aman07code <amanchoudhary07082005@gmail.com>
2026-05-24 18:32:30 +0530

    chore: add main application entry point

---
+++ src/main/java/com/adaptfit/AdaptFitApplication.java
@@ -0,0 +1,12 @@
+package com.adaptfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptFitApplication.class, args);
+    }
+}
